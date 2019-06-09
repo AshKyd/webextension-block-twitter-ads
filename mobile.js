e0823269a8ba3eb8905356e0d226ba122aa5329a
Ash Kyd <ash@kyd.com.au>
2019-06-09 21:27:04 +0200

    Update selectors for 2019-06

--- mobile.js
+++ mobile.js
@@ -4,43 +4,22 @@
  * traversing the DOM that way. It's flaky, but it works for now.
  */
 
-// The "promoted icon"
-const adSelector =
-  'path[d="M20.75 2H3.25A2.25 2.25 0 0 0 1 4.25v15.5A2.25 2.25 0 0 0 3.25 22h17.5A2.25 2.25 0 0 0 23 19.75V4.25A2.25 2.25 0 0 0 20.75 2zM17.5 13.504a.875.875 0 1 1-1.75-.001V9.967l-7.547 7.546a.875.875 0 0 1-1.238-1.238l7.547-7.547h-3.54a.876.876 0 0 1 .001-1.751h5.65c.483 0 .875.39.875.874v5.65z"]';
-
-// The dropdown chevron
-const dropdownSelector =
-  'path[d="M20.207 7.043a1 1 0 0 0-1.414 0L12 13.836 5.207 7.043a1 1 0 0 0-1.414 1.414l7.5 7.5a.996.996 0 0 0 1.414 0l7.5-7.5a1 1 0 0 0 0-1.414z"]';
-
-const debounceRate = 150;
-
-// https://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
-
 function checkAds() {
-  const isHomeTimeline = document
-    .querySelector("header")
-    .textContent.match(/^Home/);
+  // The "promoted icon"
+  var adSelector201906 =
+    '[d="M20.75 2H3.25C2.007 2 1 3.007 1 4.25v15.5C1 20.993 2.007 22 3.25 22h17.5c1.243 0 2.25-1.007 2.25-2.25V4.25C23 3.007 21.993 2 20.75 2zM17.5 13.504c0 .483-.392.875-.875.875s-.875-.393-.875-.876V9.967l-7.547 7.546c-.17.17-.395.256-.62.256s-.447-.086-.618-.257c-.342-.342-.342-.896 0-1.237l7.547-7.547h-3.54c-.482 0-.874-.393-.874-.876s.392-.875.875-.875h5.65c.483 0 .875.39.875.874v5.65z"]';
+
+  // The dropdown chevron
+  var dropdownSelector =
+    'path[d="M20.207 8.147c-.39-.39-1.023-.39-1.414 0L12 14.94 5.207 8.147c-.39-.39-1.023-.39-1.414 0-.39.39-.39 1.023 0 1.414l7.5 7.5c.195.196.45.294.707.294s.512-.098.707-.293l7.5-7.5c.39-.39.39-1.022 0-1.413z"]';
+
+  const isHomeTimeline = document.title.includes('Home / Twitter');
 
   if (!isHomeTimeline) {
     return;
   }
 
-  var ads = Array.from(document.querySelectorAll(adSelector)).map(path => {
+  var ads = Array.from(document.querySelectorAll(adSelector201906)).map(path => {
     try {
       return path.parentNode.parentNode.parentNode.parentNode.querySelector(
         dropdownSelector
@@ -62,19 +41,39 @@ function checkAds() {
   const timeout = 100;
   setTimeout(function() {
     const blockButton = Array.from(
-      document.querySelectorAll("[role=button]")
+      document.querySelectorAll("[role=menuitem]")
     ).find(btn => btn.textContent.match(/^Block @/));
     if (!blockButton) return console.log("couldn't find block button");
     blockButton.click();
     setTimeout(function() {
       const yesButton = Array.from(
         document.querySelectorAll("[role=button]")
-      ).find(btn => btn.textContent === "Yes");
+      ).find(btn => btn.textContent === "Block");
       if (!yesButton) return console.log("couldn't find yes button");
       yesButton.click();
       console.log("All done!");
     }, timeout);
   }, timeout);
+}
+
+
+const debounceRate = 150;
+
+// https://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
 }
 
 // Create an observer instance to check for & block ads
